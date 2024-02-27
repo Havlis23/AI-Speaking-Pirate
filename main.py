42755fa1c2ca4d59f0ad73de46051aa3e3ee5041
David Havel <102694838+Havlis23@users.noreply.github.com>
2024-02-27 09:51:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ from tkinter import *
 from openai import OpenAI
 import requests
 import pygame
+from dotenv import load_dotenv
+import os
 
-client = OpenAI(api_key='your-api-key-here')
+load_dotenv()
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+# klient absolutne nefahruje
+client = OpenAI(api_key=openai_api_key)
 
 root = Tk()
 root.geometry("800x300")
@@ -13,15 +20,17 @@ root.title("AI Chatbot")
 ai_response = StringVar()
 status_message = StringVar()
 
+
 def generate_content():
     status_message.set("Generating...")
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "Jsi pirát a odpovídáš jako pirát, říkáš hodně vtipů a snažíš se být co nejvíce vtipný. Dávej si pozor na skloňování v češkém jazyce. "},
+            {"role": "system",
+             "content": "Jsi pirát a odpovídáš jako pirát, říkáš hodně vtipů a snažíš se být co nejvíce vtipný. Dávej si pozor na skloňování v češkém jazyce. "},
             {"role": "user", "content": user_input.get()}
         ],
-        max_tokens=200  # The maximum number of tokens to generate in the completion
+        max_tokens=515  # The maximum number of tokens to generate in the completion
     )
     print("clovek: ", user_input.get())
     content = completion.choices[0].message.content
@@ -31,19 +40,18 @@ def generate_content():
     status_message.set("Done")
 
 
-
-
 def play_audio(file_path):
     pygame.mixer.init()
     pygame.mixer.music.load(file_path)
     pygame.mixer.music.play()
+
 
 def transcribe_to_speech(text):
     url = "https://api.elevenlabs.io/v1/text-to-speech/D38z5RcWu1voky8WS1ja"
     headers = {
         "Accept": "audio/mpeg",
         "Content-Type": "application/json",
-        "xi-api-key": "your-api-key-here"
+        "xi-api-key": "207368e9352fe73d5a1780e2b178294e"
     }
     data = {
         "text": text,  # Use the content as the value for the "text" field
@@ -63,13 +71,14 @@ def transcribe_to_speech(text):
     else:
         print(f"API request failed with status code {response.status_code}")
 
+
 # Create a StringVar to hold user input
 user_input = StringVar()
 
 # Create a label and entry field for user input
 (Label(root, text="Enter your text:",
-      bg = "white",
-       fg = "black",)
+       bg="white",
+       fg="black", )
  .pack(pady=50))
 Entry(root, textvariable=user_input,
       bg="white",
@@ -77,9 +86,7 @@ Entry(root, textvariable=user_input,
 
 Label(root, textvariable=status_message, bg="white", fg="black").pack()
 
-
 Label(root, textvariable=ai_response, bg="white", fg="black").pack()
-
 
 # Create a button to generate content
 Button(root, text="Generate", command=generate_content).pack()
